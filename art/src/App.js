944ab82222ff4c24b1136012dbd6612bdbef70b4
Louise van den Berg <louisevdb84@gmail.com>
2020-06-17 15:05:43 +0200

    added navigaion component

--- art/src/App.js
+++ art/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
 import HomePage from "./homepage/homepage.component";
+import Navigation from "./shared/navbar/navigation.component";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">      
+      <div className="App">   
+      <Navigation></Navigation>  
         <HomePage></HomePage>
       </div>
     );
--- art/src/homepage/homepage.component.jsx
+++ art/src/homepage/homepage.component.jsx
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import './homepage.style.scss';
 import ImageCarousel from './carousel/carousel.component';
 
-
 class HomePage extends Component {
   render() {
     return (
-      <div className="homepage">
+      <div className="homepage">       
         <div className="background"></div>
-        <div className="mainheading">
-          <h1>Amazing Art Page</h1>
+        <div className="mainheading">            
+          <h1>The Art Page</h1>          
           <p>Browse and make an online purchase</p>
           <ImageCarousel></ImageCarousel>
         </div>        
